a596e8dec7a24a47fbdd646064a6b2d62226d4e3
GurenMkII <62103374+GurenMkII@users.noreply.github.com>
2020-06-28 12:37:36 -0400

    Implement OrderNotFoundException class, add import for Problem from springframework. Lab Finished!

--- src/main/java/com/win/rest_api_assignment/OrderController.java
+++ src/main/java/com/win/rest_api_assignment/OrderController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.mediatype.problem.Problem;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
